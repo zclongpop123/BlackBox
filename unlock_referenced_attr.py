88b0488404e5c721089d0f61d2822106d2a9594d
Changlong.Zang <zclongpop123@163.com>
2016-01-06 18:44:06 +0800

    解锁reference物体的属性。

---
+++ unlock_referenced_attr.py
@@ -0,0 +1,14 @@
+#========================================
+# author: changlong.zang
+#   mail: zclongpop@163.com
+#   date: Fri, 21 Aug 2015, 10:32:44
+#========================================
+import pymel.core
+#--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+def unlockReferencedAttr(attribute):
+    pml_attr = pymel.core.PyNode(attribute)
+    api_attr = pml_attr.__apimplug__()
+    
+    #-
+    api_attr.setKeyable(True) 
+    api_attr.setLocked(False)
