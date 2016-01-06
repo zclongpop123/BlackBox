cf4904b3bdf5b4dfb35d7509ab4cf69a3772743e
Changlong.Zang <zclongpop123@163.com>
2016-01-06 18:43:15 +0800

    删除maya场景内不能删除的物体。

---
+++ delete_default_dagnode.py
@@ -0,0 +1,13 @@
+#========================================
+# author: changlong.zang
+#   mail: zclongpop@163.com
+#   date: Thu, 27 Aug 2015, 17:21:26
+#========================================
+import pymel.core
+import maya.OpenMaya as OpenMaya
+#--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+def deleteDefaultDagNode(object_name):
+    api_node = pymel.core.PyNode(object_name).__apimobject__()
+    api_tool = OpenMaya.MDagModifier()
+   
+    api_tool.deleteNode(api_node)
