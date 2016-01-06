bf8b20bfe9e24fcb49f5ec0821d3632690f6aee2
Changlong.Zang <zclongpop123@163.com>
2016-01-06 18:46:14 +0800

    找出物体重合的点。

---
+++ find_coincide_vertex.py
@@ -0,0 +1,28 @@
+#========================================
+# author: changlong.zang
+#   mail: zclongpop@163.com
+#   date: Sun, 06 Sep 2015, 11:23:26
+#========================================
+import md5
+import maya.cmds as mc
+#--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+def findCoincideVertex(geometry):
+    #-
+    pointCount = mc.polyEvaluate(geometry, v=True)
+    posidict  = dict()
+    for i in range(pointCount):
+        ps = mc.xform('%s.vtx[%d]'%(geometry, i), q=True, ws=True, t=True)
+
+        MD5 = md5.new()
+        MD5.update('%f%f%f'%(ps[0], ps[1], ps[2]))
+        posidict.setdefault(MD5.hexdigest(), list()).append('%s.vtx[%d]'%(geometry, i))
+    
+    #-
+    res = list()
+    for v in posidict.itervalues():
+        if len(v) < 2:
+            continue
+        res.extend(v)
+    
+    #-
+    mc.select(res)
