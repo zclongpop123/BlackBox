9f1d6f1dc1573a1e05adf4c6cc511b64d61ea515
Changlong.Zang <zclongpop123@163.com>
2016-01-14 11:21:42 +0800

    找出两个物体交错的点。

---
+++ find_intersect_vertex.py
@@ -0,0 +1,55 @@
+#========================================
+# author: changlong.zang
+#   mail: zclongpop@163.com
+#   date: Thu, 14 Jan 2016, 11:15:49
+#========================================
+import pymel.core
+import maya.OpenMaya as OpenMaya
+#--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+def find_intersect_vertex(obj_A, obj_b):
+    '''
+    '''
+    src_pml_node = pymel.core.PyNode(obj_A)
+    dst_pml_node = pymel.core.PyNode(obj_b)
+
+    src_dag_path = src_pml_node.__apiobject__()
+    dst_dag_path = dst_pml_node.__apiobject__()
+
+    src_mfn_node = OpenMaya.MFnMesh(src_dag_path)
+    iterator     = OpenMaya.MItGeometry(dst_dag_path)
+
+    comp_list = list()
+    closest_point = OpenMaya.MPoint()
+    farray = OpenMaya.MFloatPointArray()
+    for i in xrange(iterator.count()):
+        farray.clear()
+
+        current_point = iterator.position(OpenMaya.MSpace.kWorld)
+        src_mfn_node.getClosestPoint(current_point, closest_point, OpenMaya.MSpace.kWorld)
+
+        point  = OpenMaya.MFloatPoint(current_point.x,  current_point.y, current_point.z)
+        vector = OpenMaya.MFloatVector(closest_point.x, closest_point.y, closest_point.z)
+
+        src_mfn_node.allIntersections(point, vector, None, None,
+                                      False, OpenMaya.MSpace.kWorld,
+                                      10000, False, None, False,
+                                      farray, None, None, None, None, None)
+        if farray.length() % 2 == 1:
+            comp_list.append(iterator.index())
+        iterator.next()
+
+    api_util = OpenMaya.MScriptUtil()
+    api_util.createFromList(comp_list, len(comp_list))
+    int_ptr  = api_util.asIntPtr()
+    id_array = OpenMaya.MIntArray(int_ptr, len(comp_list))
+
+    single_components = OpenMaya.MFnSingleIndexedComponent()
+    vertex_components = single_components.create(OpenMaya.MFn.kMeshVertComponent)
+    single_components.addElements(id_array)
+
+    mSelectionList = OpenMaya.MSelectionList()
+    mSelectionList.add(dst_dag_path, vertex_components)
+    OpenMaya.MGlobal.setActiveSelectionList(mSelectionList)
+
+if __name__ == '__main__':
+    find_intersect_vertex('pSphere1', 'pSphere2')
